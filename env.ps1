a597f3becbf1f97022f5bd2982d6c198c8d9f570
Edison Garcia <egachi@live.com>
2019-10-28 12:41:00 -0500

    Updating to Github

--- env.ps1
+++
@@ -1,4 +0,0 @@
-$Env:DBHOST = "edisga.postgres.database.azure.com"
-$Env:DBUSER = "mredison"
-$Env:DBNAME = "pollsdb"
-$Env:DBPASS = "Pa$$w0rd123456"
--- env.sh
+++
@@ -1,5 +0,0 @@
-export DBHOST="edisga.postgres.database.azure.com"
-export DBUSER="mredison@edisga"
-export DBNAME="pollsdb"
-export DBPASS="Pa$$w0rd123456"
-
